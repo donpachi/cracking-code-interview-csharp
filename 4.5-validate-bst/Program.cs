69ff3255c8898c32e8450ee26e338bb9657927a4
Aaron Kim <aaron.th.kim@gmail.com>
2018-04-06 17:01:32 -0600

    4.5 valid bst started, contemplating tests

---
+++ 4.5-validate-bst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4._5_validate_bst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
